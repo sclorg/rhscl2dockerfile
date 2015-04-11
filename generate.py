470bca50b2e747c95ce4c0a77c9c879bb723f299
Honza Horak <hhorak@redhat.com>
2015-04-11 20:28:31 +0200

    Refactoring whole generator

--- generate.py
+++ generate.py
@@ -7,67 +7,113 @@ import sys
 import shutil
 
 
-def copy(src, dst):
-    if os.path.isdir(src):
-        if os.path.exists(dst):
-            shutil.rmtree(dst)
-        shutil.copytree(src, dst)
-    else:
-        shutil.copy(src, dst)
+class CollectionGenerator:
+    def __init__(self, env, cvars, coll, tname, tvars):
+        self.cvars = cvars
+        self.env = env
+        self.collection = coll
+        self.tname = tname
+        self.tvars = tvars
+
+    def copy_render(self, src, dst):
+        """
+        Copy file or dir recursively from src to dst while file content
+        is rendered as template using general values and values specific
+        for collection.
+        """
+        if os.path.isdir(src):
+            if not os.path.isdir(dst):
+                os.makedirs(dst)
+            files = os.listdir(src)
+            for f in files:
+                self.copy_render(os.path.join(src, f), 
+                                 os.path.join(dst, f))
+        else:
+            # Construct a template, render dst, write, done.
+            with open(src, 'r') as f:
+                temp = self.env.from_string(f.read())
+            with open(dst, "w") as f:
+                f.write(temp.render(self.cvars))
+ 
+
+    def handle_add_files(self):
+        """
+        Handle files that are added to Dockerfile using ADD command.
+        These files are copied into dockerfile directory and content is used
+        as template, so possible template macros are expanded.
+        Add distro-specific files, distro-agnostic files are already in cvars
+        if defined
+        """
+        add_key = 'add.{}'.format(self.tname)
+        if add_key in self.cvars:
+            add_files = self.cvars[add_key]
+        elif 'add' in self.cvars:
+            add_files = self.cvars['add']
+        else:
+            add_files = []
+
+        # Set correct src and dst values
+        result = []
+        for f in add_files:
+            try:
+                [src, dst_file, dst_dir] = f.split()
+                # we use dst_file here since that is how the file is called
+                # in the dockerfile directory
+                result.append({'src': os.path.join('.', dst_file),
+                               'dst': os.path.join(dst_dir, dst_file)})
+                self.copy_render(src, os.path.join(self.outdir, dst_file))
+            except ValueError:
+                print('Error: added files spec has incorrect format: {}'.format(f))
+        self.cvars['add_files'] = result
 
 
-def get_add_files(cvars, tname):
-    # Add distro-specific files, distro-agnostic files are already in cvars
-    # if defined
-    add_key = 'add.{}'.format(tname)
-    if add_key in cvars:
-        add_files = cvars[add_key]
-    elif 'add' in cvars:
-        add_files = cvars['add']
-    else:
-        add_files = []
-
-    # Set correct src and dst values
-    result = []
-    for f in add_files:
-        try:
-            [src, dst_file, dst_dir] = f.split()
-            # we use dst_file here since that is how the file is called
-            # in the dockerfile directory
-            result.append({'src': os.path.join('.', dst_file),
-                           'dst': os.path.join(dst_dir, dst_file)})
-            copy(src, os.path.join(outdir, dst_file))
-        except ValueError:
-            print('Error: added files spec has incorrect format: {}'.format(f))
-    return result
+    def set_defaults(self):
+        """
+        Set up collection variables which can be substituted in templates
+        """
+        self.cvars["container"] = self.collection
+        if 'collection' not in self.cvars:
+            self.cvars["collection"] = self.collection
+        if 'enable' not in self.cvars:
+            self.cvars["enable"] = [self.cvars["collection"]]
 
 
-y = yaml.safe_load(open('rhscl.yaml'))
+    def generate(self, outdir):
+        """
+        Generates Dockerfile and files for one collection
+        """
+        self.outdir = outdir
+        self.set_defaults()
+        self.handle_add_files()
+        for src, dst in self.tvars.items():
+            # Allow use of variables in dst filenames as well as files
+            dst = Template(dst).render(self.cvars)
+            self.copy_render(src, os.path.join(self.outdir, dst))
+            print("wrote %s for %s on %s" % (dst, self.collection, self.tname))
 
-env = Environment(keep_trailing_newline=True)
 
-for coll, cvars in y["containers"].items():
-    for tname, tvars in y["templates"].items():
-        outdir = tname + "." + coll
-        try:
-            os.makedirs(outdir)
-        except:
-            pass
-        # Set up collection variables which can be substituted in templates
-        cvars["container"] = coll
-        if 'collection' not in cvars:
-            cvars["collection"] = coll
-        if 'enable' not in cvars:
-            cvars["enable"] = [cvars["collection"]]
-        cvars['add_files'] = get_add_files(cvars, tname)
+class DockerfileGenerator:
+    def __init__(self, config_file):
+        self.y = yaml.safe_load(open(config_file))
+        self.env = Environment(keep_trailing_newline=True)
 
-        for template, output in tvars.items():
-            # Allow use of variables in output filenames as well as files
-            output = os.path.join(outdir, Template(output).render(cvars))
-            # Construct a template, render output, write, done.
-            temp = env.from_string(open(template, "r").read())
-            outp = temp.render(cvars)
-            open(output, "w").write(outp)
-             
-            print("wrote %s for %s on %s" % (output, coll, tname))
-	    	
+    def generate_all(self):
+        """
+        Loops through all collections in the config file and generates
+        Dockerfile and other files for every collections specified.
+        """
+        for coll, cvars in self.y["containers"].items():
+            for tname, tvars in self.y["templates"].items():
+                outdir = tname + "." + coll
+                try:
+                    os.makedirs(outdir)
+                except:
+                    pass
+                collection = CollectionGenerator(self.env, cvars, coll, tname, tvars)
+                collection.generate(outdir)
+
+
+if __name__ == "__main__":
+    generator = DockerfileGenerator('rhscl.yaml')
+    generator.generate_all()
+
