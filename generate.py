25e0002b0928405d61476390e43955d4c2de7aee
Honza Horak <hhorak@redhat.com>
2015-04-11 17:42:40 +0200

    Add support for adding external files

--- generate.py
+++ generate.py
@@ -4,6 +4,43 @@ from jinja2 import Environment, Template
 import yaml
 import os
 import sys
+import shutil
+
+
+def copy(src, dst):
+    if os.path.isdir(src):
+        if os.path.exists(dst):
+            shutil.rmtree(dst)
+        shutil.copytree(src, dst)
+    else:
+        shutil.copy(src, dst)
+
+
+def get_add_files(cvars, tname):
+    # Add distro-specific files, distro-agnostic files are already in cvars
+    # if defined
+    add_key = 'add.{}'.format(tname)
+    if add_key in cvars:
+        add_files = cvars[add_key]
+    elif 'add' in cvars:
+        add_files = cvars['add']
+    else:
+        add_files = []
+
+    # Set correct src and dst values
+    result = []
+    for f in add_files:
+        try:
+            [src, dst_file, dst_dir] = f.split()
+            # we use dst_file here since that is how the file is called
+            # in the dockerfile directory
+            result.append({'src': os.path.join('.', dst_file),
+                           'dst': os.path.join(dst_dir, dst_file)})
+            copy(src, os.path.join(outdir, dst_file))
+        except ValueError:
+            print('Error: added files spec has incorrect format: {}'.format(f))
+    return result
+
 
 y = yaml.safe_load(open('rhscl.yaml'))
 
@@ -22,9 +59,11 @@ for coll, cvars in y["containers"].items():
             cvars["collection"] = coll
         if 'enable' not in cvars:
             cvars["enable"] = [cvars["collection"]]
+        cvars['add_files'] = get_add_files(cvars, tname)
+
         for template, output in tvars.items():
             # Allow use of variables in output filenames as well as files
-            output = outdir + "/" + Template(output).render(cvars)
+            output = os.path.join(outdir, Template(output).render(cvars))
             # Construct a template, render output, write, done.
             temp = env.from_string(open(template, "r").read())
             outp = temp.render(cvars)
