caab879f7bc883383958abae1338ea0e8ee16ab0
Marek Skalický <mskalick@redhat.com>
2015-04-30 15:05:51 +0200

    Add MongoDB shard script and README

--- mongodb/cont-mongos-daemon.sh
+++ mongodb/cont-mongos-daemon.sh
@@ -43,10 +43,20 @@ function wait_mongo() {
 # Shutdown mongod on SIGINT/SIGTERM
 function cleanup() {
     echo "=> Shutting down MongoDB server"
-    if [ -s $dbpath/mongod.lock ]; then
-        mongod $mongo_common_args --shutdown
+    if [ -s $pidfile ]; then
+        set +e
+        kill $(cat $pidfile)
+        set -e
     fi
     wait_mongo "DOWN"
+    status=$?
+
+    if [ $status -ne 0 -a -s $pidfile ]; then
+        set +e
+        kill -9 $(cat $pidfile)
+        set -e
+        wait_mongo "DOWN"
+    fi
 
     exit 0
 }
@@ -54,31 +64,25 @@ function cleanup() {
 MAX_ATTEMPTS=90
 SLEEP_TIME=2
 
-mongod_config_file="/etc/mongod.conf"
+mongos_config_file="/etc/mongos.conf"
 
-# Change config file according MONGOD_CONFIG_* variables
-for option in $(set | grep MONGOD_CONFIG | sed -r -e 's|MONGOD_CONFIG_||'); do
+# Change config file according MONGOS_CONFIG_* variables
+for option in $(set | grep MONGOS_CONFIG | sed -r -e 's|MONGOS_CONFIG_||'); do
     # Delete old option from config file
     option_name=$(echo $option | sed -r -e 's|(\w*)=.*|\1|')
-    sed -r -e "/^$option_name/d" $mongod_config_file > $HOME/.mongod.conf
-    cat $HOME/.mongod.conf > $mongod_config_file
-    rm $HOME/.mongod.conf
+    sed -r -e "/^$option_name/d" $mongos_config_file > $HOME/.mongos.conf
+    cat $HOME/.mongos.conf > $mongos_config_file
+    rm $HOME/.mongos.conf
     # Add new option into config file
-    echo $option >> $mongod_config_file
+    echo $option >> $mongos_config_file
 done
 
 # Get options from config file
-dbpath=$(grep dbpath $mongod_config_file | sed -r -e 's|dbpath\s*=\s*||')
+pidfile=$(grep '^\s*pidfilepath' $mongos_config_file | sed -r -e 's|^\s*pidfilepath\s*=\s*||')
 
 # Get used port
-port=27017
-if grep '^\s*port' $mongod_config_file &>/dev/null; then
-    port=$(grep '^\s*port' $mongod_config_file | sed -r -e 's|^\s*port\s*=\s*(\d*)|\1|')
-elif grep '^\s*configsvr' $mongod_config_file &>/dev/null; then
-    port=27019
-elif grep '^\s*shardsvr' $mongod_config_file &>/dev/null; then
-    port=27018
-fi
+port=$(grep '^\s*port' $mongos_config_file | sed -r -e 's|^\s*port\s*=\s*(\d*)|\1|')
+port=${port:-27017}
 
 trap 'cleanup' SIGINT SIGTERM
 
@@ -86,22 +90,33 @@ trap 'cleanup' SIGINT SIGTERM
 source_scripts mongodb pre-init
 
 # Add default config file
-mongo_common_args="-f $mongod_config_file "
+mongo_common_args="-f $mongos_config_file "
 mongo_local_args="--bind_ip localhost "
 
 # Start background MongoDB service with disabled authentication
-mongod $mongo_common_args $mongo_local_args &
+mongos $mongo_common_args $mongo_local_args &
 wait_mongo "UP"
 
 # Run scripts with started mongod
 source_scripts mongodb init
 
-# Stop background MongoDB service to exec mongod
-mongod $mongo_common_args $mongo_local_args --shutdown
+# Stop background MongoDB service to exec mongos
+set +e
+kill $(cat $pidfile)
+set -e
 wait_mongo "DOWN"
+status=$?
+
+if [ $status -ne 0]; then
+    set +e
+    kill -9 $(cat $pidfile)
+    set -e
+    wait_mongo "DOWN"
+fi
+
 
 # Run scripts after mongod stoped
 source_scripts mongodb post-init
 
 # Start MongoDB service with enabled authentication
-exec mongod $mongo_common_args
+exec mongos $mongo_common_args
